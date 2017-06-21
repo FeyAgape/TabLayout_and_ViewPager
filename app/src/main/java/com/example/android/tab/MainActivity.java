9143c50cb198b677af5fe26ac32a8c9021466200
Fey Ijaware <fshola4real@yahoo.co.uk>
2017-06-21 20:09:48 +0100

    Updated Content

--- app/src/main/java/com/example/android/tab/MainActivity.java
+++ app/src/main/java/com/example/android/tab/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.android.tab;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
-import layout.SampleFragmentPagerAdapter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,11 +19,50 @@ public class MainActivity extends AppCompatActivity {
 
         // Get the ViewPager and set it's PagerAdapter so that it can display items
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        viewPager.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager(),
-                MainActivity.this));
+        ViewPagerAdapter adapter = new ViewPagerAdapter (getSupportFragmentManager());
 
-        // Give the TabLayout the ViewPager
+        // Add Fragments to adapter one by one
+        adapter.addFragment(new One(), getString(R.string.Tab_one) );
+        adapter.addFragment(new Two(), getString(R.string.Tab_two));
+        adapter.addFragment(new Three(), getString(R.string.Tab_three));
+        adapter.addFragment(new Four(), getString(R.string.Tab_four));
+        adapter.addFragment(new Five(), getString(R.string.Tab_five));
+        viewPager.setAdapter(adapter);
+
+
         TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
     }
+
+
+    // Adapter for the viewpager using FragmentPagerAdapter
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
 }
+
---
+++ app/src/main/java/com/example/android/tab/Three.java
@@ -0,0 +1,27 @@
+package com.example.android.tab;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Three extends Fragment {
+
+    public Three() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_three, container, false);
+    }
+
+}
